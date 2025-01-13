770247089ee30a012612feb4f48e503d06b89a1b
luorongrong <luorongrong@meituan.com>
2025-01-13 19:39:03 +0800

    feat: add react demo

---
+++ vue3/src/components/counter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    {{ count }}
+    <button @click="handleAdd">+1</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0);
+
+const handleAdd = () => { count.value++ }
+
+</script>
