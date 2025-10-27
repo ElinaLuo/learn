37307ed57782e4b51b5d3cb23333bc2c6588c93f
luorongrong <luorongrong@meituan.com>
2025-10-27 14:04:42 +0800

    feat: commit

--- es6/debounce.js
+++ es6/debounce.js
@@ -1,29 +1,44 @@
-function debounce(fn, wait, immediate = false) {
-  let timer = null
 
-  return function(...args) {
-    clearTimeout(timer)
+/**
+ * 防抖
+ * @param {Function} fn 
+ * @param {Number} wait 
+ * @param {Object} leading 是否立即执行，默认为 false
+ * @param {Object} trailing 是否在最后一次调用后延迟执行，默认为 true
+ * @returns 
+ */
+function debounce(fn, wait, { leading = false, trailing = true } = {}) {
+  let timeoutId = null
+
+  const cancel = function() {
+    clearTimeout(timeoutId)
+    timeoutId = null
+  }
+  const debounced = function(...args) {
+    // 每次调用都重新计时
+    clearTimeout(timeoutId)
     // 立即执行
-    if(immediate) {
-      if (!timer) {
+    if(leading && !timeoutId) {
+      fn.apply(this, args)
+    }
+    timeoutId = setTimeout(() => {
+      if (trailing) {
         fn.apply(this, args)
       }
-      setTimeout(() => {
-        timer = null
-      }, wait)
-    } else {
-      timer = setTimeout(() => {
-        fn.apply(this, args)
-      }, wait)
-    }
+      cancel()
+    }, wait)
   }
+  debounced.cancel = cancel
+  return debounced
 }
 
-let count = 100
-// 多次调用，只触发最后一次
-const fn = debounce(() => {
-  console.log('click')
-}, 100)
-while(count--) {
-  fn()
-}
+const fn = debounce((msg) => {
+  console.log('click', msg)
+}, 1000, { leading: true, trailing: true })
+fn('第1次调用')
+fn('第2次调用')
+fn('第3次调用')
+setTimeout(() => fn('第4次调用'), 400)
+setTimeout(() => fn('第5次调用'), 900)
+setTimeout(() => fn('第6次调用'), 2000)
+setTimeout(() => fn('第7次调用'), 2200)
---
+++ es6/es-toolkit_debounce.js
@@ -0,0 +1,13 @@
+import { debounce } from 'es-toolkit';
+
+const fn = debounce((msg) => {
+  console.log('click', msg)
+}, 1000, { edges: ['leading', 'trailing'] })
+fn('第1次调用')
+fn('第2次调用')
+fn('第3次调用')
+setTimeout(() => fn('第4次调用'), 400)
+setTimeout(() => fn('第5次调用'), 900)
+setTimeout(() => fn('第6次调用'), 2000)
+setTimeout(() => fn('第7次调用'), 2200)
+
