f56bfbeeb1ed56c80402c467a3f95250266f45df
luorongrong <luorongrong@meituan.com>
2025-11-14 18:45:29 +0800

    ss

--- js/Array/flat.js
+++ js/Array/flat.js
@@ -1,11 +1,18 @@
 /**
  * @description 实现数组的flat
  */
-Array.prototype._flat = function() {
-    return this.reduce((pre, cur, index) => {
-        return Array.isArray(cur) ? [...pre, ...cur._flat()] : [...pre, cur]
-    }, [])
-}
+Array.prototype._flat = function (depth = 1) {
+  return this.reduce((prev, cur) => {
+    if (Array.isArray(cur) && depth > 0) {
+      prev.push(...cur._flat(depth - 1));
+    } else {
+      prev.push(cur);
+    }
+    return prev;
+  }, []);
+};
 
-let arr = [1, 2, [3, 4], [5, [6, 7]]]
-console.log(arr._flat())
+let arr = [1, 2, [3, 4], [5, [6, [7, 8, [9]]]]];
+console.log(arr._flat(1));
+console.log(arr._flat(3));
+console.log(arr._flat(4));
